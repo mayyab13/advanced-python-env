74932fa045105cc601bdb9602b4fc3dd00dddb82
mayyab13 <mayya.b13@gmail.com>
2025-12-14 17:38:12 +0500

    task1.py

--- task1.py
+++ task1.py
@@ -1,8 +1,9 @@
-first_name = input("Your first name? ")
-last_name = input("Your last name? ")
+last_first = input("Your last name, first name? ")
+last_name, first_name = last_first.split(", ")
+
 age = input("How old are you? ")
 phone = input("Your phone number? ")
 
-print(f"Your first name, last name: {first_name} {last_name}")
-print(f"Your age: {age}")
-print(f"Your phone number: {phone}")
+print(f"{first_name} {last_name}")
+print(age)
+print(phone)
